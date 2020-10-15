95ab53b761577ef6ee6064cf11e209b68d5870cd
Ryo-15 <tomorrows.way15@gmail.com>
2020-10-15 17:08:34 +0900

    [finish]stop_watch_js_lesson_8

--- js/main.js
+++ js/main.js
@@ -23,14 +23,40 @@
     }, 10);
   }
 
+  // ボタンの状態を設定(初期)
+  function setButtonStateInitial() {
+    start.disabled = false;
+    stop.disabled = true;
+    reset.disabled = true;
+  }
+
+  // ボタンの状態を設定(カウント中)
+  function setButtonStateRunning() {
+    start.disabled = true;
+    stop.disabled = false;
+    reset.disabled = true;
+  }
+
+  // ボタンの状態を設定(停止時)
+  function setButtonStateStopped() {
+    start.disabled = false;
+    stop.disabled = true;
+    reset.disabled = false;
+  }
+
+  // デフォルト設定
+  setButtonStateInitial();
+
   // カウントアップ機能
   start.addEventListener('click', () => {
+    setButtonStateRunning()
     startTime = Date.now();
     countUp();
   });
 
   // カウントストップ機能
   stop.addEventListener('click', () => {
+    setButtonStateStopped()
     clearTimeout(timeoutId);
     // カウント再開時の設定
     elapsedTime += Date.now() - startTime;
@@ -38,6 +64,7 @@
 
   // カウントリセット機能
   reset.addEventListener('click', () => {
+    setButtonStateInitial()
     timer.textContent = '00:00.000';
     elapsedTime = 0;
   });
